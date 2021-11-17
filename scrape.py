c30f19a495d8b1222663ece15404634ccffddc74
sajarindider <sajarindider@gmail.com>
2021-11-17 18:08:03 -0500

    feat: implement saving pages from xtools

--- scrape.py
+++ scrape.py
@@ -3,20 +3,23 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.webdriver import WebDriver
 
-def configure_page(base_url: str, search_query: str) -> WebDriver:
+def configure_page(base_url: str, search_query: str) -> str:
     driver = webdriver.Chrome()
     driver.get(base_url)
     assert "Page History" in driver.title
+    assert "Error" not in driver.title
     search_box = driver.find_element(By.ID, 'article_input')
     search_box.clear()
     search_box.send_keys(search_query)
     submit_btn = driver.find_element(By.CLASS_NAME, 'form-submit')
     submit_btn.click()
-    return driver.page_source
+    page_source = driver.page_source
+    driver.close()
+    return page_source
 
 def save_page(coin_name: str) -> None:
     edits_url = 'https://xtools.wmflabs.org/articleinfo/en.wikipedia.org'
     edits_page_source = configure_page(edits_url, coin_name)
     pathlib.Path(f'data/{coin_name}/saved_pages/').mkdir(parents=True, exist_ok=True)
-    with open(f'data/{coin_name}/saved_pages/edits.html', 'w', encoding="utf-8") as f:
+    with open(f'data/{coin_name}/saved_pages/edits.html', 'w+', encoding="utf-8") as f:
         f.write(edits_page_source)
